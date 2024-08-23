1be70a9351de8df2ed0c7cff282a5df9cb061ddb
ale <alemoura.zyc@gmail.com>
2024-08-23 19:54:40 -0300

    chore: add aula02

---
+++ aula02/index.js
@@ -0,0 +1,8 @@
+// escrever um comentário em linha
+console.log('Olá mundo!'); // aqui tem outro comentário
+console.log('Linha 1');
+
+/* comentário em escopo
+console.log('Linha 2);
+console.log('Linha 3);
+*/

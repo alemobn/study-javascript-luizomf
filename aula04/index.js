76ce05bb3b29fce1c79ba23df5ad670e985c825d
ale <alemoura.zyc@gmail.com>
2024-08-23 19:55:12 -0300

    chore: add aula04

---
+++ aula04/index.js
@@ -0,0 +1,39 @@
+// variáveis precisam ter nomes significativos
+// não pode começar o nome de uma variável com número
+// não podem conter espaços ou traços
+// utilizamos camelCase
+// case-sensitive
+// não podemos redeclarar variáveis com let
+// não utilize var, utilize let
+let nomeCliente = 'Luiz';
+nomeCliente = 'Otávio';
+
+console.log(nomeCliente);
+
+/*
+não podemos criar variáveis com palavras reservadas
+let console, let alert, let let, let var, let const, etc..
+*/
+
+/*
+let nome; // variável declarada (retorna undefined)
+nome = 'João'; // inicializando a variável
+console.log(nome);
+nome = 'Otávio'; // atribuindo um novo valor a mesma variável
+console.log(nome);
+*/
+
+/*
+a variável não pode ser redeclarada
+let nome;
+*/
+
+/*
+let nome = 'Henrique'; // String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com',nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+*/

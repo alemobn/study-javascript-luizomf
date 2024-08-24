cc41c2381c0d41848bcb9aa82fdb129776776b11
ale <alemoura.zyc@gmail.com>
2024-08-24 18:19:12 -0300

    chore: add aula07

---
+++ aula07/index.js
@@ -0,0 +1,19 @@
+// string, number, undefined, null, boolean, symbol
+
+const nome = 'Luiz'; // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+const num = 10; // number
+const num1 = 10.52; // number
+let nomeAluno; // undefined = não aponta pra nenhum local na meḿoria
+let sobrenomeAluno = null; // nulo = não aponta pra nenhum local na memória
+const aprovado = true; // boolean = true or false (lógico)
+
+let a = 2;
+const b = a;
+
+console.log(a, b);
+
+a = 3;
+
+console.log(a, b);

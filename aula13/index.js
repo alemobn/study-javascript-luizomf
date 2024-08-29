77fcefbf6a56c2bd9922294cde04362ed03bee95
ale <alemoura.zyc@gmail.com>
2024-08-28 22:25:51 -0300

    chore: add aula13

---
+++ aula13/index.js
@@ -0,0 +1,18 @@
+let num1 = 9.52343;
+let raizQuadrada = 9;
+
+console.log(100 / 0); // retorna o tipo Infinity e retorna True (dividir um número por 0 pode dar erro em outras linguagens de programação)
+
+console.log(raizQuadrada ** (1/2)); // raiz quadrada de RaizQuadrada
+console.log(raizQuadrada ** 0.5); // raiz quadrada de RaizQuadrada
+
+console.log(Math.floor(num1)); // arredonda para baixo (para número inteiro)
+console.log(Math.ceil(num1)); // arredonda para cima (para número inteiro)
+console.log(Math.round(num1)); // arredonda para o mais próximo (para número inteiro)
+console.log(Math.max(1,2,3,4,5,-10,-60,70,1600,-234)); // retorna o maior número da sequência
+console.log(Math.min(1,2,3,4,5,-10,-60,70,1600,-234)); // retorna o menor número da sequência
+console.log(Math.random()); // retorna um número aleatório de 0 a 1 (o 1 não é incluido)
+console.log(Math.round(Math.random())); // arredonda automaticamente o resultado 
+console.log(Math.random() * (10 - 5) + 5); // retorna um número aleatório entre 10 e 5
+console.log(Math.PI); // valor de PI
+console.log(Math.pow(2, 10)); // potenciacão, 2 elevado a 10 (utilize o operador de potenciação)

44632f7ff5a69e3978851996199a6803824c508f
ale <alemoura.zyc@gmail.com>
2024-08-30 22:00:31 -0300

    chore: add aula20

---
+++ aula20/index.js
@@ -0,0 +1,17 @@
+/**
+ * operadores de comparação
+> maor que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) ********** não recomendável
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) ********** não recomendável
+!== diferente estrito (valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = 10;
+const comp = num1 !== num2;
+
+console.log(comp);

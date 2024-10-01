9d62bd156989465f9b25126408e73df32a95f9da
ale <alemoura.zyc@gmail.com>
2024-10-01 15:09:17 -0300

    chore: add ex

---
+++ aula38/ex/index.js
@@ -0,0 +1,20 @@
+// escreva uma função que recebe um número e retorne o seguinte:
+// Número é divisível por 3 = Fizz
+// Número é divisível por 5 = Buzz
+// Número é divisível por 3 e 5 = FizzBuzz
+// Número NÃO é divisível por 3 e 5 = Retorna o próprio número
+// Checar se o número é realmente um número = Retorna o próprio número
+// Use a função com números de 0 a 100
+
+const isDivsible = (num) => {
+    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+    if (num % 3 === 0) return 'Fizz';
+    if (num % 5 === 0) return 'Buzz';
+    if (typeof num !== 'number') return num;
+    return num;
+}
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i, isDivsible(i));
+}
+    
